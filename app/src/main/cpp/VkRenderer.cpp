9b7e25dabc09260ed72ed17d258268c9b80a581d
HuhChangMu <huhcm@pubg.com>
2024-07-15 23:22:02 +0900

    vulkan-instance

--- app/src/main/cpp/VkRenderer.cpp
+++ app/src/main/cpp/VkRenderer.cpp
@@ -12,16 +12,35 @@
 using namespace std;
 
 VkRenderer::VkRenderer() {
-    uint32_t  layerCount;
-    VK_CHECK_ERROR(vkEnumerateInstanceLayerProperties(&layerCount, nullptr));
 
-    vector<VkLayerProperties> layerProps(layerCount);
-    VK_CHECK_ERROR(vkEnumerateInstanceLayerProperties(&layerCount, layerProps.data()));
+    VkApplicationInfo applicationInfo{
+        .sType = VK_STRUCTURE_TYPE_APPLICATION_INFO,
+        .pApplicationName = "Vulkan Ex",
+        .applicationVersion = VK_MAKE_API_VERSION(0, 0, 1, 0),
+        .apiVersion = VK_MAKE_API_VERSION(0, 1, 3, 0)
+    };
 
-    for (const auto& layerProp : layerProps) {
-        aout << layerProp.layerName << endl;
+    uint32_t  instanceLayerCount;
+    VK_CHECK_ERROR(vkEnumerateInstanceLayerProperties(&instanceLayerCount, nullptr));
+
+    vector<VkLayerProperties> instancLayerProperties(instanceLayerCount);
+    VK_CHECK_ERROR(vkEnumerateInstanceLayerProperties(&instanceLayerCount, instancLayerProperties.data()));
+
+    vector<const char*> instanceLayerNames;
+    for (const auto& layerProperty : instancLayerProperties) {
+        instanceLayerNames.push_back(layerProperty.layerName);
     }
+
+    VkInstanceCreateInfo instanceCreateInfo{
+        .sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
+        .pApplicationInfo = &applicationInfo,
+        .enabledLayerCount = instanceLayerCount,
+        .ppEnabledLayerNames = instanceLayerNames.data()
+    };
+
+    VK_CHECK_ERROR(vkCreateInstance(&instanceCreateInfo, nullptr, &mInstance));
 }
 
 VkRenderer::~VkRenderer() {
+    vkDestroyInstance(mInstance, nullptr);
 }
