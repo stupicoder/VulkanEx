52b1b835e3eb575be9dcbc6c9b7783ff10f1245f
StupidCoder <huhcm4796@gmail.com>
2025-06-09 02:30:03 +0900

    hello-vulkan

--- Practice/app/src/main/cpp/main.cpp
+++ Practice/app/src/main/cpp/main.cpp
@@ -1,10 +1,11 @@
 #include <jni.h>
 
 #include "AndroidOut.h"
-#include "Renderer.h"
 
 #include <game-activity/GameActivity.cpp>
 #include <game-text-input/gametextinput.cpp>
+
+#include <vulkan/vulkan.h>
 
 extern "C" {
 
@@ -18,23 +19,9 @@ extern "C" {
 void handle_cmd(android_app *pApp, int32_t cmd) {
     switch (cmd) {
         case APP_CMD_INIT_WINDOW:
-            // A new window is created, associate a renderer with it. You may replace this with a
-            // "game" class if that suits your needs. Remember to change all instances of userData
-            // if you change the class here as a reinterpret_cast is dangerous this in the
-            // android_main function and the APP_CMD_TERM_WINDOW handler case.
-            pApp->userData = new Renderer(pApp);
+            aout << "Hello Vulkan!" << std::endl;
             break;
         case APP_CMD_TERM_WINDOW:
-            // The window is being destroyed. Use this to clean up your userData to avoid leaking
-            // resources.
-            //
-            // We have to check if userData is assigned just in case this comes in really quickly
-            if (pApp->userData) {
-                //
-                auto *pRenderer = reinterpret_cast<Renderer *>(pApp->userData);
-                pApp->userData = nullptr;
-                delete pRenderer;
-            }
             break;
         default:
             break;
@@ -103,15 +90,6 @@ void android_main(struct android_app *pApp) {
 
         // Check if any user data is associated. This is assigned in handle_cmd
         if (pApp->userData) {
-            // We know that our user data is a Renderer, so reinterpret cast it. If you change your
-            // user data remember to change it here
-            auto *pRenderer = reinterpret_cast<Renderer *>(pApp->userData);
-
-            // Process game input
-            pRenderer->handleInput();
-
-            // Render a frame
-            pRenderer->render();
         }
     } while (!pApp->destroyRequested);
 }
