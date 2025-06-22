ad629098bfb8f257298a129f29ef73d75a334685
StupidCoder <huhcm4796@gmail.com>
2025-06-22 20:32:00 +0900

    windows project

--- PracticeWin/sources/main.cpp
+++ PracticeWin/sources/main.cpp
@@ -1,6 +1,91 @@
+#define UNICODE
+#include <windows.h>
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, Vulkan!" << std::endl;
+#ifdef _DEBUG
+void CreateConsole()
+{
+    if (AllocConsole())
+    {
+        FILE* pCout;
+        freopen_s(&pCout, "CONOUT$", "w", stdout);
+        SetConsoleTitle(L"Debug Console");
+    }
+}
+#endif
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
+{
+#ifdef _DEBUG
+    CreateConsole();
+#endif
+
+    // Register the window class.
+    const wchar_t CLASS_NAME[]  = L"Sample Window Class";
+    
+    WNDCLASS wc = { };
+
+    wc.lpfnWndProc   = WindowProc;
+    wc.hInstance     = hInstance;
+    wc.lpszClassName = CLASS_NAME;
+
+    RegisterClass(&wc);
+
+    // Create the window.
+    HWND hwnd = CreateWindowEx(
+        0,                              // Optional window styles.
+        CLASS_NAME,                     // Window class
+        L"Vulkan Window",    // Window text
+        WS_OVERLAPPEDWINDOW,            // Window style
+
+        // Size and position
+        CW_USEDEFAULT, CW_USEDEFAULT, 800, 600,
+
+        NULL,       // Parent window    
+        NULL,       // Menu
+        hInstance,  // Instance handle
+        NULL        // Additional application data
+        );
+
+    if (hwnd == NULL)
+    {
+        return 0;
+    }
+
+    std::cout << "Window created successfully. Console is attached in Debug mode." << std::endl;
+
+    ShowWindow(hwnd, nCmdShow);
+
+    // Run the message loop.
+    MSG msg = { };
+    while (GetMessage(&msg, NULL, 0, 0) > 0)
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
     return 0;
+}
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    switch (uMsg)
+    {
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+
+    case WM_PAINT:
+        {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hwnd, &ps);
+            FillRect(hdc, &ps.rcPaint, (HBRUSH) (COLOR_WINDOW+1));
+            EndPaint(hwnd, &ps);
+        }
+        return 0;
+
+    }
+    return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
